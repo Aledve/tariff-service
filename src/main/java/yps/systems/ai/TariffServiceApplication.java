92ff711b3e77c6af3ffb08b91e1b60bfb82c63bd
Aledve <jakile971012@gmail.com>
2026-01-19 11:14:52 -0500

    Rename ObjectiveServiceApplication to TariffServiceApplication

--- src/main/java/yps/systems/ai/TariffServiceApplication.java
+++ src/main/java/yps/systems/ai/TariffServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ObjectiveServiceApplication {
+public class TariffServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ObjectiveServiceApplication.class, args);
+        SpringApplication.run(TariffServiceApplication.class, args);
     }
 
 }
