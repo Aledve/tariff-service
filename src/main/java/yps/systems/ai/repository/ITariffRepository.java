fdc6dfefe40ffee298c36cc86179d84e82c06038
Aledve <jakile971012@gmail.com>
2026-01-20 13:23:28 -0500

    🧹refactor(repo): rename ObjectiveRepository to TariffRepository

--- src/main/java/yps/systems/ai/repository/ITariffRepository.java
+++ src/main/java/yps/systems/ai/repository/ITariffRepository.java
@@ -2,8 +2,8 @@ package yps.systems.ai.repository;
 
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
-import yps.systems.ai.model.Objective;
+import yps.systems.ai.model.Tariff;
 
 @Repository
-public interface IObjectiveRepository extends Neo4jRepository<Objective, String> {
+public interface ITariffRepository extends Neo4jRepository<Tariff, String> {
 }
